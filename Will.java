61e773bd79c7a17625d7ef06a607ce8e966ea039
willgabriel136 <williamlindao130@gmail.com>
2023-02-28 14:57:07 +0000

    Salvar

---
+++ Will.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Will {
+   
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+        int num;
+
+        System.out.println("Insira um numero! :");
+        num = input.nextInt();
+      
+        if (num > 0){
+            System.out.print ("O numero é positivo!");
+        }else if( num < 0){
+
+            System.out.print ("O numero é negativo!");
+        }else 
+        {
+            System.out.print ("O numero é 0!");
+        }
+    }
+}
---
+++ Will2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Will2 {
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+
+        String nome;
+        int idade;
+     
+        System.out.println("Informe seu nome : "); 
+        nome = input.next();
+    
+        System.out.println("Informe a sua idade : "); 
+        idade = input.nextInt();
+
+        if (idade >= 0 && idade <= 12)
+        {
+            System.out.print("Você é criança!");
+
+        }else if (idade >= 13 && idade <= 17){
+                System.out.print("Você é adolescente!");
+
+        }else if (idade >= 18 && idade <= 59){
+            System.out.print("Você é adulto!");
+
+        }else if(idade >60){
+            System.out.print("Você é idoso!");
+    }
+    }
+}
