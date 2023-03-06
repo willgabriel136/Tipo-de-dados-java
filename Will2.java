d1c0c7c307a05a25f9a58090cb2837f26bb99df2
willgabriel136 <williamlindao130@gmail.com>
2023-03-06 11:11:06 +0000

    salvar

--- Will2.java
+++ Will2.java
@@ -16,16 +16,16 @@ public class Will2 {
 
         if (idade >= 0 && idade <= 12)
         {
-            System.out.print("Você é criança!");
+            System.out.print("Você é criança! \n");
 
         }else if (idade >= 13 && idade <= 17){
-                System.out.print("Você é adolescente!");
+                System.out.print("Você é adolescente! \n");
 
         }else if (idade >= 18 && idade <= 59){
-            System.out.print("Você é adulto!");
+            System.out.print("Você é adulto! \n");
 
         }else if(idade >60){
-            System.out.print("Você é idoso!");
+            System.out.print("Você é idoso! \n");
     }
     }
 }
