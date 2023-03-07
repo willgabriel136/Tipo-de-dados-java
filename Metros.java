049a5312385ecedbcc978847820f9b6e25393b2b
willgabriel136 <williamlindao130@gmail.com>
2023-03-07 14:51:00 +0000

    salvar

--- Metros.java
+++ Metros.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class Metros {
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        float m, c ;
 
+        System.out.println("Digite um número : ");
+        m = input.nextFloat();
 
-        
+       c = m * 100;
+       System.out.printf("convertido para cm " + c + "\n");
+    
+
 }
 }
