da63af7f854c8d1655ebc164646ebb1019e7b427
willgabriel136 <williamlindao130@gmail.com>
2023-03-07 14:17:27 +0000

    salvar

--- Peso.java
+++ Peso.java
@@ -4,31 +4,40 @@ public class Peso {
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        double Peso, Altura, imc;
+        double altura, peso, imc;
 
-         
-        System.out.println("Dígite sua altura:");
-        Altura = input.nextDouble();
-        System.out.println("Dígite seu peso:");
-        Peso = input.nextDouble();
+        System.out.println("Digite seu peso: "); 
+        peso = input.nextDouble();
 
-        imc = Peso / (Altura * Altura);
-
-        if (Peso < 18,5)
-        {
-            System.out.print("Abaixo do Peso \n");
-        } else if (idade >= 13 && idade <= 17){
-            System.out.print("Adolescente \n");
-        } else if (idade >= 18 && idade <= 59){
-            System.out.print("Adulto \n");
-        } else if (idade > 60){
-            System.out.print("idoso \n");
-        }
-
-
-        System.out.printf("Seu peso é: " + Peso + "\n");
-        System.out.printf("\n Sua altura é: " + Altura + "\n");
-
+        System.out.println("Digite sua altura: "); 
+        altura = input.nextDouble(); 
+ 
+        imc = peso / (altura * altura);
         
-    }
+        if (imc < 18.5) {
+            System.out.printf("o seu imc e" + "a faixa de peso correspondente é: abaixo do peso");
+        } else if (imc <= 24.9){    
+
+           System.out.printf("o seu imc e" + "a faixa de peso correspondente é: Peso normal ");
+        } else if (imc <= 29.9){    
+     
+            System.out.printf("o seu imc e" + "a faixa de peso correspondente é: Sobrepeso");
+        } else if (imc <= 34.9){    
+         
+            System.out.printf("o seu imc e" + "a faixa de peso correspondente é: Obesidade grau 1");
+        } else if (imc <= 39.9){    
+
+
+            System.out.printf("o seu imc e" + "a faixa de peso correspondente é: Obesidade grau 2");
+        } else if (imc <= 34.9){   
+
+
+
+
+}
+}
+
+
+
+
 }
