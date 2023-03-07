1e317b1ba2a2427cd55159d521a6bf4f9e0bf703
willgabriel136 <williamlindao130@gmail.com>
2023-03-07 14:12:12 +0000

    salvar

--- SalarioDoWill.java
+++ SalarioDoWill.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 
 public class SalarioDoWill {
     public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    double SalarioAtual, SalarioNovo ;
 
-        Scanner input = new Scanner(System.in);
-        double SalarioAtual, SalarioNovo;
-
-        System.out.println("Informe seu Salário Atual: "); 
+        System.out.println("Informe seu salário atual: " );
         SalarioAtual = input.nextDouble();
 
-        if (SalarioAtual > 1000 && SalarioAtual < 2000) {
-            SalarioNovo = SalarioAtual * 1.15 ;
-            System.out.println("Seu novo salário é:  \n" + SalarioNovo);
-            
-        }else if (SalarioAtual > 2000 && SalarioAtual < 3000) {
-            SalarioNovo = SalarioAtual * 1.10;
-            System.out.println("Seu novo salário é:  \n" + SalarioNovo);
 
-        }else if (SalarioAtual >= 3000) {
+        if (SalarioAtual >= 1000 && SalarioAtual < 2000){
+            SalarioNovo = SalarioAtual * 1.15;
+            System.out.println("seu salário Novo é: " + SalarioNovo);
+            
+
+        }
+        else if (SalarioAtual >= 2000 && SalarioAtual <= 3000){
+            SalarioNovo = SalarioAtual * 1.10;
+            System.out.println("seu salário Novo é: " + SalarioNovo);
+      
+        }
+        else if (SalarioAtual >= 3000){
             SalarioNovo = SalarioAtual * 1.05;
-            System.out.println("Seu novo salário é:  \n" + SalarioNovo);
+            System.out.println("seu salário Novo é: " + SalarioNovo);
+            
         }
     }
 }
