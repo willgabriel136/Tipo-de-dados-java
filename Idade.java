f9defdfcb1950ce2524c157b72deabb03cbf03a4
willgabriel136 <williamlindao130@gmail.com>
2023-03-07 15:16:54 +0000

    salvar

---
+++ Idade.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Idade {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        String n1, n2;
+        int idade1, idade2;
+        
+
+
+        System.out.println("Digite seu nome :");
+        n1 = input.next();
+
+        System.out.println("Digite seu nome :");
+        n2 = input.next(); 
+
+        System.out.println("Digite sua idade :");
+        idade1= input.nextInt();
+
+        System.out.println("Digite usa Idade :");
+        idade2 = input.nextInt();
+
+        
+        if (idade1 > idade2);
+        {
+            System.out.printf("Pessoa mais velha é a " + n1);
+
+      
+
+
+}
+}
+}
